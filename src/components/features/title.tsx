355d3a47efedd743439e0214b3d0e516101e36ed
Jeroen Reumkens <hello@jeroenreumkens.nl>
2023-05-19 09:50:48 +0200

    Manually set the root element for useinview. This is ONLY needed when running this code inside an iframe.

--- src/components/features/title.tsx
+++ src/components/features/title.tsx
@@ -10,7 +10,23 @@ type Props = {
 
 export const FeatureTitle = ({ children, id }: Props) => {
   const ref = useRef<HTMLParagraphElement>(null);
-  const isInView = useInView(ref, { margin: "-50% 0px -50% 0px" });
+  const documentRef = useRef(document);
+  const isInView = useInView(ref, {
+    margin: "-50% 0px -50% 0px",
+    // NOTE: The only reason we pass in the document here, is because
+    // of security restrictions set by the browser when using an iFrame.
+    // In an iFrame (so eg in the preview on frontend.fyi),
+    // margin won't take effect unless you specify the root manually.
+    // By default it will be the window element, which is what we want in this case.
+    // If you specify your own root, you can usually only pass in an Element, and
+    // not the document (since document/window is the default). However, in order
+    // to fix the issue in the iframe, we need to pass in the document here and thus
+    // tell TypeScript that we know what we're doing. If you're implementing
+    // this in your own website, you can just pass in the root property as well as the documentRef.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    root: documentRef,
+  });
   const setInViewFeature = useFeatureStore((state) => state.setInViewFeature);
   const inViewFeature = useFeatureStore((state) => state.inViewFeature);
 
